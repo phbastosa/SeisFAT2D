8c02621cf3ff9965c925de770f31c8c8867ae1fb
Paulo H. B. Alves <44127778+phbastosa@users.noreply.github.com>
2024-11-12 18:34:14 -0300

    Update adjoint_state.cuh

--- src/inversion/adjoint_state.cuh
+++ src/inversion/adjoint_state.cuh
@@ -16,9 +16,6 @@ private:
     float * m = nullptr;
     float * v = nullptr;
 
-    float * m_hat = nullptr;
-    float * v_hat = nullptr;
-
     float * d_T = nullptr;
 
     float * d_source_grad = nullptr;
